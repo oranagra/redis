997c3ec8ab6ce34183eb3e1c71f6f5794a789d09
Oran Agra <oran@redislabs.com>
2019-10-28 15:06:57 +0200

    break tests

--- tests/unit/type/string.tcl
+++ tests/unit/type/string.tcl
@@ -2,7 +2,7 @@ start_server {tags {"string"}} {
     test {SET and GET an item} {
         r set x foobar
         r get x
-    } {foobar}
+    } {foobar_junk}
 
     test {SET and GET an empty item} {
         r set x {}
